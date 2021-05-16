8a22b0c40e22e5921ff96169a6380de65ae9301c
Ahmetnarin <ahmetnarin568@gmail.com>
2021-05-16 18:40:26 +0300

    finished

--- todo-list/script.js
+++ todo-list/script.js
@@ -13,12 +13,15 @@ while (input !== 'quit' && input !== 'q' ){
         todos.push(newTodo);
         console.log(`${newTodo} added to list!`);
     } else if(input === "delete"){
-        const index = prompt("Ok, enter an index to delete!");
-        console.log(`${index} : ${todos[index]} is deleted`);
-        todos.splice(index, 1);
-        
+        const index = parseInt(prompt("Ok, enter an index to delete!"));
+        if (!Number.isNaN(index) && index < todos.length){
+            const deleted = todos.splice(index , 1);
+            console.log(`Ok, deleted ${deleted[0]}`);
+        } else {
+            console.log("Unknown index");
+        }
     }
-    input = prompt("What would like to do?")
+    input = prompt("What would like to do?");
 }
 
-console.log("OK, YOU QUIT THE APP!")
+console.log("OK, YOU QUIT THE APP!");
