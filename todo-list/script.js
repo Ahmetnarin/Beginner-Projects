ec2b2e8b93a4f16b13055f3773ca433bcddd51f6
Ahmetnarin <ahmetnarin568@gmail.com>
2021-05-16 16:38:21 +0300

    teste

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,8 +1,24 @@
 let input = prompt("What would like to do?");
-const todos = [''];
+const todos = ['Collect Chicken Eggs' , 'Clean Litter Box'];
 
-while(input !== 'quit' && input !== 'q'){
-    input = prompt("What would like to do?");
+while (input !== 'quit' && input !== 'q' ){
+    if (input === 'list'){
+        console.log('*******************');
+        for(let i =0 ; i < todos.length; i++){
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('*******************');
+    }else if(input ==="new"){
+        const newTodo = prompt("OK, What is new todo?");
+        todos.push(newTodo);
+        console.log(`${newTodo} added to list!`);
+    } else if(input === "delete"){
+        const index = prompt("Ok, enter an index to delete!");
+        console.log(`${index} : ${todos[index]} is deleted`);
+        todos.splice(index, 1);
+        
+    }
+    input = prompt("What would like to do?")
 }
 
 console.log("OK, YOU QUIT THE APP!")
