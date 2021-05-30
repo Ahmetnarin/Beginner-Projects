67ead334008adbb22014d3cbabd138623158f53b
Ahmetnarin <ahmetnarin568@gmail.com>
2021-05-31 01:17:21 +0300

    my solution

--- score_keeper/app.js
+++ score_keeper/app.js
@@ -1,49 +1,67 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
-const reset = document.querySelector('#reset');
+const resetButton = document.querySelector('#reset');
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
+let isGameOver = false;
 
 
 p1Button.addEventListener('click', function () {
-    if (p1Score !== winningScore){
+    if(!isGameOver){
         p1Score += 1;
-        p1Display.textContent = p1Score;
+        if(p1Score === winningScore){
+            isGameOver = true;
+            if (p1Score > p2Score) {
+                p1Display.classList.add('winner');
+                p2Display.classList.add('loser');
 
-    }else {
-        p1Button.disabled = true;
-        p2Button.disabled = true;
-    }
-    
+            }
+        }
+        p1Display.textContent = p1Score;
+        
+    }    
 })
 
 
 p2Button.addEventListener('click', function () {
-    if(p2Score !== winningScore){
+    if (!isGameOver) {
         p2Score += 1;
+        if (p2Score === winningScore) {
+            isGameOver = true;
+            if(p1Score < p2Score){
+                p2Display.classList.add('winner');
+                p1Display.classList.add('loser');
+
+            }
+            
+            
+        }
         p2Display.textContent = p2Score;
-    }else {
-        p1Button.disabled = true;
-        p2Button.disabled = true;
     }
-    
 })
 
-reset.addEventListener('click', function () {
-    // if(reset){
-        p1Score = 0;
-        p2Score = 0;
-        p1Display.textContent = p1Score;
-        p2Display.textContent = p2Score;
-        p1Button.disabled = false;
-        p2Button.disabled = false;
-
-
-    // }
-    
-
+winningScoreSelect.addEventListener('change', function (){
+    // alert(this.value);
+    winningScore = parseInt(this.value);
+    reset();
 })
+
+resetButton.addEventListener('click', reset) ;
+
+function reset() {
+    isGameOver = false;
+    p1Score = false;
+    p2Score = false;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+
+    p1Display.classList.remove('winner');
+    p1Display.classList.remove('loser');
+    p2Display.classList.remove('winner');
+    p2Display.classList.remove('loser');
+}
