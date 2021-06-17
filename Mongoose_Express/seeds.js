113454b0a0286e811baf013f62c46dcb97cfbeca
Ahmetnarin <ahmetnarin568@gmail.com>
2021-06-17 12:34:18 +0300

    seeds file

--- Mongoose_Express/seeds.js
+++ Mongoose_Express/seeds.js
@@ -16,10 +16,47 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
         category: 'fruit'
     })
 
-p.save()
-    .then(p => {
-        console.log(p);
-    })
-    .catch(e => {
-        console.log(e)
-    })
+// p.save()
+//     .then(p => {
+//         console.log(p);
+//     })
+//     .catch(e => {
+//         console.log(e)
+//     })
+
+const seedProducts = [
+    {
+        name: 'Fairy Eggplant',
+        price: 1.00,
+        category: 'vegetable'
+    },
+    {
+        name: 'Organic Goddess Melon',
+        price: 4.99,
+        category: 'fruit'
+    },
+    {
+        name: 'Organic Mini Seedless Watermelon',
+        price: 3.99,
+        category: 'fruit'
+    },
+    {
+        name: 'Organic Celery',
+        price: 1.50,
+        category: 'vegetable'
+    },
+    {
+        name: 'Chocolate Whole Milk',
+        price: 2.68,
+        category: 'dairy'
+    }
+
+]
+
+Product.insertMany(seedProducts )
+.then(res => {
+    console.log(res);
+})
+.catch(e => {
+    console.log(e);
+} )
