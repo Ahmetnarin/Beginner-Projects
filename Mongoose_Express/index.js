a6bba2170c62b235a5f7cd34ec49625f66c9bb0e
Ahmetnarin <ahmetnarin568@gmail.com>
2021-06-17 15:26:36 +0300

    files to upload

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -16,11 +16,15 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine' , 'ejs');
+app.use(express.urlencoded({extended: true}))
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
     console.log(products)
     res.render('products/index' , { products })
+})
+app.get('/products/new' , (req, res) => {
+    res.render('products/new')
 })
 
 app.get('/products/:id', async (req, res) => {
@@ -28,6 +32,14 @@ app.get('/products/:id', async (req, res) => {
     const product  = await Product.findById(id);
     console.log(product);
     res.render('products/show', { product } );
+})
+
+// poster
+app.post('/products' , async (req,res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    console.log(newProduct);
+    res.redirect(`/products/${newProduct._id}` )
 })
 
 
