4877ad0cef89cbdfce867bdfae0ecdc3fe26f770
Ahmetnarin <ahmetnarin568@gmail.com>
2021-06-20 14:51:36 +0300

    files to upload

--- Middleware/index.js
+++ Middleware/index.js
@@ -6,34 +6,28 @@ const morgan = require("morgan");
 
 app.use(morgan('tiny'));
 app.use((req, res, next) => {
-    console.log(req.method.toUpperCase(), req.pathWOOF);
-    next()
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
 })
 
 
-
-
-
-
-
-// app.use((req, res, next) => {
-//     console.log("THIS IS MY FIRST MIDDLEWARE");
-//     next();
-// })
-// app.use((req, res, next) => {
-//     console.log("THIS IS MY SECOND MIDDLEWARE");
-//     next();
-// })
-
 app.get("/" , (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`);
     res.send("Home Page!");
     console.log("This is the Homepage")
 })
 
 app.get("/dogs" , (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`);
     res.send("WOOF WOOF!");
     console.log("Here is a dog which is barking")
 })
+
+app.use((req, res) => {
+    res.status(404).send('NOT FOUND!!');
+})
+
 
 app.listen(3000, () => {
     console.log("App is running on localhost:3000");
