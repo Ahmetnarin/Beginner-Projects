2cb8888a3d5891afc5ba22f1e95109c469117332
Ahmetnarin <ahmetnarin568@gmail.com>
2021-06-20 14:33:23 +0300

    learning express middleware

--- Middleware/index.js
+++ Middleware/index.js
@@ -4,15 +4,26 @@ const morgan = require("morgan");
 
 
 
-app.use(morgan('common'));
+app.use(morgan('tiny'));
 app.use((req, res, next) => {
-    console.log("THIS IS MY FIRST MIDDLEWARE");
-    next();
+    console.log(req.method.toUpperCase(), req.pathWOOF);
+    next()
 })
-app.use((req, res, next) => {
-    console.log("THIS IS MY SECOND MIDDLEWARE");
-    next();
-})
+
+
+
+
+
+
+
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY FIRST MIDDLEWARE");
+//     next();
+// })
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY SECOND MIDDLEWARE");
+//     next();
+// })
 
 app.get("/" , (req, res) => {
     res.send("Home Page!");
