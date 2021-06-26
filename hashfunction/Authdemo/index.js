00cb712498bf7278af1faca0c3c7973a7ed3c0a2
Ahmetnarin <ahmetnarin568@gmail.com>
2021-06-26 20:44:12 +0300

    sign up function with hashfunction

--- hashfunction/Authdemo/index.js
+++ hashfunction/Authdemo/index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN");
+    })
+    .catch(err => {
+        console.log("MONGO CONNECTION ERROR");
+        console.log(err);
+    })
+
 
 app.set('view engine' , 'ejs');
 app.set('views' , 'views');
 
+app.use(express.urlencoded({extended : true}));
+
 app.get('/register' , (req, res) => {
     res.render('register');
+})
+
+app.post("/register", async (req, res) => {
+    res.send(req.body);
 })
 
 
