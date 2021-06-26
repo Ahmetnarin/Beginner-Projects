4461a760c9a67f41d278ec34ad7d0a5bdb341b71
Ahmetnarin <ahmetnarin568@gmail.com>
2021-06-26 22:46:11 +0300

    login page

--- hashfunction/Authdemo/index.js
+++ hashfunction/Authdemo/index.js
@@ -3,7 +3,7 @@ const app = express();
 const User = require('./models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const session = required('express-session');
+const session = require('express-session');
 
 
 mongoose.connect('mongodb://localhost:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -20,7 +20,11 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true }));
-app.use(session({secret: 'notagoodsecret'}));
+app.use(session({
+    secret: 'notagoodsecret',
+    resave: false, 
+    // saveUninitialized: false
+}));
 
 app.get('/', (req, res) => {
     res.send("THIS IS THE HOMEPAGE!!")
@@ -42,27 +46,37 @@ app.post("/register", async (req, res) => {
         password: hash
     })
     await user.save();
+    req.session.user_id = user._id;
     res.redirect('/')
 })
 
 app.get('/login', (req, res) => {
     res.render('login')
 })
+
+
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const user = await User.findOne({ username })
     const validPassword = await bcrypt.compare(password, user.password)
     if (validPassword) {
         req.session.user_id = user._id;
-        res.send("Welcome!")
-    } else {
-        res.send("Incorrect username or password! Try again...");
+        res.redirect('/secret');
+    } 
+    else {
+        res.redirect('/login')
     }
 })
 
 
-app.get('/secret', (req, res) => {
-    res.send('THIS IS SECRET! YOU CANNOT SEE ME UNLESS YOU ARE LOGGED IN!');
+app.get('/secret', (req, res, next) => {
+    if(!req.session.user_id){
+        // res.redirect('/login');
+        res.send('THIS IS SECRET! YOU CANNOT SEE ME UNLESS YOU ARE LOGGED IN!');
+        // next();
+    }
+    
+    res.send('Now you can see secret files!!')
 })
 
 app.listen(3000, () => {
